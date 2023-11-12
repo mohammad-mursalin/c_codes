2f4ea645683408fb2da3c11d92695dca6b1c3ad3
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-12 23:59:00 +0600

    C-Tuitorial.c

---
+++ C-Tuitorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main()
+{
+    /*
+    int x;
+    
+    x = 5;
+
+    printf("%d\n",x);
+
+    return 0;
+    */
+
+   int n;
+
+   printf("Enter a number : ");
+
+    scanf("%d",&n);
+
+    printf("n = %d",n);
+}
