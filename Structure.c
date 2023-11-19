d5bcb49290dd535311e007a3bd0f07206fcf42b5
mohammad-mursalin <mursalinlamon@gmail.com>
2023-11-19 23:12:12 +0600

    structure

--- Structure.c
+++ Structure.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 
-struct Person
+struct Student
 {
-    char name[20];
+    int class;
+    char section;
     int age;
     double weight;
 };
 
 int main()
 {
-    struct Person p1;
-    p1 = {21,60,"Rafi"};
+    struct Student s1,s2,s3,s4,s5;
 
-    printf("Name : %s",p1.name);
+        s1.class = 5;
+        s1.section = 'a';
+        s1.age = 15;
+        s1.weight = 40;
+
+    printf("Student 1 Information\n");
+    printf("class : %d\nSection : %c\nAge : %d\nWeight : %d",s1.class,s1.section,s1.age,s1.weight);
+
+    s2 = {6,'a',16,45};
+    
+
 
 }
