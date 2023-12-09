af3ebe4c04e77e8f0df1ad5a5ba439355c4d2436
mohammad_mursalin <mursalinlamon@gmail.com>
2023-12-09 23:48:35 +0600

    String palindrome

---
+++ StringPalindrome.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char name[30];
+
+    printf("Enter a name : ");
+    gets(name);
+
+    int len = strlen(name);
+
+    char temp[30];
+
+    int i,j;
+
+    for(i = 0, j = len-1; i<len,j>=0;i++,j--)
+    {
+        temp[j] = name[i];
+    }
+    
+
+    int c = strcmp(name,temp);
+
+    if(c==0)
+    printf("palindrome");
+
+    else
+    printf("not");
+
+    
+}
